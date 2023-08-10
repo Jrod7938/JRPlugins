a7509d2df83bd792ae6ac326771f6cb104ef8969
ByronHutch <ostalkhcim@gmail.com>
2023-08-10 03:02:56 -0400

    update e3t4g with break handler support & toggle & running status overlay

--- src/main/java/com/example/E3t4g/ThreeTickFourGranite.java
+++ src/main/java/com/example/E3t4g/ThreeTickFourGranite.java
@@ -10,34 +10,83 @@ import com.example.Packets.MousePackets;
 import com.example.Packets.ObjectPackets;
 import com.example.Packets.WidgetPackets;
 import com.google.inject.Inject;
+import com.google.inject.Provides;
+import com.piggyplugins.PiggyUtils.BreakHandler.ReflectBreakHandler;
+import lombok.Getter;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
 import net.runelite.api.ItemID;
 import net.runelite.api.VarPlayer;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.widgets.Widget;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.input.KeyManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDependency;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+import net.runelite.client.util.HotkeyListener;
 
 import java.util.Optional;
 
 @PluginDescriptor(
-        name = "3t4g",
+        name = "<html><font color=\"#FF9DF9\">[EV]</font> 3t4g</html>",
         enabledByDefault = false
 )
 @PluginDependency(EthanApiPlugin.class)
 @PluginDependency(PacketUtilsPlugin.class)
-public class e3t4g extends Plugin {
+public class ThreeTickFourGranite extends Plugin {
     @Inject
     Client client;
+    @Inject
+    private ReflectBreakHandler breakHandler;
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private KeyManager keyManager;
+    @Inject
+    private ThreeTickFourGraniteConfig config;
+    @Inject
+    private ThreeTickFourGraniteOverlay overlay;
+    @Getter
+    boolean started;
     int[][] rockPos = new int[][]{{3165, 2908}, {3165, 2909}, {3165, 2910}, {3167, 2911}};
     int timeout = 0;
     int rock = 0;
 
+    @Provides
+    private ThreeTickFourGraniteConfig getConfig(ConfigManager configManager) {
+        return configManager.getConfig(ThreeTickFourGraniteConfig.class);
+    }
+
+    @Override
+    protected void startUp() throws Exception {
+        breakHandler.registerPlugin(this);
+        keyManager.registerKeyListener(toggle);
+        overlayManager.add(overlay);
+    }
+
+    @Override
+    protected void shutDown() throws Exception {
+        breakHandler.unregisterPlugin(this);
+        keyManager.unregisterKeyListener(toggle);
+        overlayManager.remove(overlay);
+    }
+
     @Subscribe
     public void onGameTick(GameTick e) {
+        if (client.getGameState() != GameState.LOGGED_IN || !started
+            || breakHandler.isBreakActive(this)) {
+            return;
+        }
+
+        if (breakHandler.shouldBreak(this)) {
+            breakHandler.startBreak(this);
+            return;
+        }
+
         timeout = timeout == 0 ? 2 : timeout - 1;
         if (timeout != 2) return;
         if (client.getVarpValue(VarPlayer.SPECIAL_ATTACK_PERCENT) == 1000) {
@@ -84,5 +133,26 @@ public class e3t4g extends Plugin {
         Inventory.search().nameContains("Granite").first().ifPresent(item -> InventoryInteraction.useItem(item, "Drop"));
         MousePackets.queueClickPacket();
         ObjectPackets.queueObjectAction(1, 11387, rockPos[rock][0], rockPos[rock][1], false);
+    }
+
+    private final HotkeyListener toggle = new HotkeyListener(() -> config.e3t4gToggle()) {
+        @Override
+        public void hotkeyPressed() {
+            toggle();
+        }
+    };
+
+
+    public void toggle() {
+        if (client.getGameState() != GameState.LOGGED_IN) {
+            return;
+        }
+        started = !started;
+
+        if (!started) {
+            breakHandler.stopPlugin(this);
+        } else {
+            breakHandler.startPlugin(this);
+        }
     }
 }
