64d04c890627e23df53c20773259391cb9254160
Jancarlos Rodriguez <jrod7938@gmail.com>
2024-02-03 22:43:08 -0500

    AutoChop Config Kotlin->Java

---
+++ AutoChop/src/main/java/com/jrplugins/AutoChop/AutoChopConfig.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024. By Jrod7938
+ *
+ */
+
+package com.jrplugins.AutoChop;
+
+import com.jrplugins.AutoChop.enums.TreeAndLocation;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.Keybind;
+
+@ConfigGroup("AutoChop")
+public interface AutoChopConfig extends Config {
+    @ConfigItem(
+            keyName = "Toggle",
+            name = "Toggle",
+            description = "",
+            position = 0
+    )
+    default Keybind toggle() { return Keybind.NOT_SET; }
+
+    @ConfigItem(
+            keyName = "overlay",
+            name = "Overlay",
+            description = "Display Overlay?",
+            position = 1
+    )
+    default Boolean displayOverlay() { return true; }
+
+    @ConfigItem(
+            keyName = "burnLogs",
+            name = "Burn Logs?",
+            description = "Burn Logs?",
+            position = 2
+    )
+    default Boolean burnLogs() { return false; }
+
+    @ConfigItem(
+            keyName = "treeName",
+            name = "Tree Name",
+            description = "Tree to cut",
+            position = 3
+    )
+    default TreeAndLocation TREEANDLOCATION() { return TreeAndLocation.Maple; }
+
+}
