b4f6469b703088cb405fcfdfdae25bd11b5878f5
jarromie <jarromie@whocodes.tech>
2024-03-20 13:05:55 -0400

    Add Break Handler support to Power Skiller (#45)
    * Add Break Handler support to Power Skiller
    
    * Remove duplicate statement

--- PowerSkiller/src/main/java/com/piggyplugins/PowerSkiller/PowerSkillerPlugin.java
+++ PowerSkiller/src/main/java/com/piggyplugins/PowerSkiller/PowerSkillerPlugin.java
@@ -8,6 +8,7 @@ import com.example.InteractionApi.InventoryInteraction;
 import com.example.InteractionApi.NPCInteraction;
 import com.example.InteractionApi.TileObjectInteraction;
 import com.google.inject.Provides;
+import com.piggyplugins.PiggyUtils.BreakHandler.ReflectBreakHandler;
 import net.runelite.api.*;
 import net.runelite.api.coords.WorldPoint;
 import net.runelite.api.events.GameTick;
@@ -40,6 +41,8 @@ public class PowerSkillerPlugin extends Plugin {
     @Inject
     private KeyManager keyManager;
     @Inject
+    private ReflectBreakHandler breakHandler;
+    @Inject
     private OverlayManager overlayManager;
     @Inject
     private PowerSkillerOverlay overlay;
@@ -53,6 +56,7 @@ public class PowerSkillerPlugin extends Plugin {
     @Override
     protected void startUp() throws Exception {
         bankPin = false;
+        breakHandler.registerPlugin(this);
         keyManager.registerKeyListener(toggle);
         this.overlayManager.add(overlay);
     }
@@ -60,6 +64,7 @@ public class PowerSkillerPlugin extends Plugin {
     @Override
     protected void shutDown() throws Exception {
         bankPin = false;
+        breakHandler.unregisterPlugin(this);
         keyManager.unregisterKeyListener(toggle);
         this.overlayManager.remove(overlay);
     }
@@ -71,7 +76,7 @@ public class PowerSkillerPlugin extends Plugin {
 
     @Subscribe
     private void onGameTick(GameTick event) {
-        if (!EthanApiPlugin.loggedIn() || !started) {
+        if (!EthanApiPlugin.loggedIn() || !started || breakHandler.isBreakActive(this)) {
             // We do an early return if the user isn't logged in
             return;
         }
@@ -300,9 +305,14 @@ public class PowerSkillerPlugin extends Plugin {
     }
 
     public void toggle() {
-        if (client.getGameState() != GameState.LOGGED_IN) {
+        if (!EthanApiPlugin.loggedIn()) {
             return;
         }
         started = !started;
+        if(started){
+            breakHandler.stopPlugin(this);
+        }else{
+            breakHandler.startPlugin(this);
+        }
     }
 }
