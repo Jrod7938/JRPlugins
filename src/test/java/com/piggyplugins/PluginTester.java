1a51853641ed4254ddc4efc0aabffd14c21f365e
Junkie <102834696+Blunkie@users.noreply.github.com>
2024-05-28 22:39:38 -0400

    Version bumping RuneLite.. We have removed Cannon Loader to prioritize our efforts towards the upcoming premium release as well as other standalone plugins. If you wish to see Cannon Loader back we encourage you to submit a pull request. We appreciate your understanding and continued support.

--- src/test/java/com/piggyplugins/PluginTester.java
+++ src/test/java/com/piggyplugins/PluginTester.java
@@ -3,7 +3,6 @@ package com.piggyplugins;
 import com.example.EthanApiPlugin.EthanApiPlugin;
 import com.example.PacketUtils.PacketUtilsPlugin;
 import com.example.PrayerFlicker.EthanPrayerFlickerPlugin;
-import com.piggyplugins.CannonReloader.CannonReloaderPlugin;
 import com.piggyplugins.LeftClickBlackJack.LeftClickBlackJackPlugin;
 import com.piggyplugins.PiggyUtils.PiggyUtilsPlugin;
 import com.piggyplugins.strategyexample.StrategySmithPlugin;
@@ -19,7 +18,7 @@ public class PluginTester {
                 PiggyUtilsPlugin.class// Don't remove these
         /* Add your plugins in this method when running from the IDE.
            Make sure to include them as a dependency in the build.gradle via `testImplementation` */,
-                StrategySmithPlugin.class, AutoBonerPlugin.class, CannonReloaderPlugin.class, EthanPrayerFlickerPlugin.class, LeftClickBlackJackPlugin.class);
+                StrategySmithPlugin.class, AutoBonerPlugin.class, EthanPrayerFlickerPlugin.class, LeftClickBlackJackPlugin.class);
         RuneLite.main(args);
     }
 }
